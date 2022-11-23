88e2c702f2b336ec081344ba39d8b0d61ee5129c
Valerii <vvv.gassiev@gmail.com>
2022-11-23 17:22:03 +0300

    add new

--- lib/pages/intro_page.dart
+++ lib/pages/intro_page.dart
@@ -1,59 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'home_page.dart';
 
-class IntroPage extends StatelessWidget {
-  const IntroPage({Key? key}) : super(key: key);
+class IntroScreen extends StatelessWidget {
+  const IntroScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Padding(
-            padding: const EdgeInsets.only(left: 80, right: 80, bottom: 40, top: 160),
-            child: Image.asset('lib/images/avocado.png'),
-          ),
+      backgroundColor: Colors.brown[200],
+      body: SafeArea(
+        child: Column(
+          children: [
+            // big logo
+            Padding(
+              padding: const EdgeInsets.only(
+                left: 100.0,
+                right: 100.0,
+                top: 120,
+                bottom: 20,
+              ),
+              child: Image.asset('lib/images/avocado.png'),
+            ),
 
-          const Padding(
-            padding: EdgeInsets.all(24.0),
-            child: Text(
-              "We deliver goods at your doorstep",
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                fontSize: 40,
-                fontWeight: FontWeight.bold,
+
+            Padding(
+              padding: const EdgeInsets.all(28.0),
+              child: Text(
+                'We deliver groceries at your doorstep',
+                textAlign: TextAlign.center,
+                style: GoogleFonts.montserrat(
+                    fontSize: 36,
+                    color: Colors.white,
+                ),
               ),
             ),
-          ),
-
-          const Text(
-            "Fresh items every day",
-            style: TextStyle(
-              fontSize: 20,
-            ),
-          ),
 
 
-          const Spacer(),
-
-
-
-          Container(
-            decoration: BoxDecoration(
-              color: Colors.brown[200],
-              borderRadius: BorderRadius.circular(12),
-            ),
-            padding: EdgeInsets.all(12),
-            child: const Text(
-              'Get started',
+            Text(
+              'Fresh items everyday',
+              textAlign: TextAlign.center,
               style: TextStyle(
+                fontSize: 16,
                 color: Colors.white,
               ),
             ),
-          ),
 
-          const Spacer(),
+            const SizedBox(height: 24),
 
-        ],
+            const Spacer(),
+
+            // get started button
+            GestureDetector(
+              onTap: () => Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(
+                  builder: (context) {
+                    return HomePage();
+                  },
+                ),
+              ),
+              child: Container(
+                padding: const EdgeInsets.all(24),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.brown[400],
+                ),
+                child: const Text(
+                  "Get Started",
+                  style: TextStyle(
+                    color: Colors.white,
+                    // fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+
+            const Spacer(),
+          ],
+        ),
       ),
     );
   }
